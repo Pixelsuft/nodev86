7a2a0e465863b2c69978d5613018214535cabdac
Pixelsuft <pixelsuft@gmail.com>
2022-06-11 19:14:40 +0700

    updates

--- src/acpi.cpp
+++ src/acpi.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <math.h>
 #include <sys/time.h>
 #include <extern_api.h>
 #define PMTIMER_FREQ_SECONDS 3579545
@@ -7,36 +8,39 @@
 
 using namespace std;
 
-uint16_t status = 1;
-uint16_t pm1_status = 0;
-uint16_t pm1_enable = 0;
-uint32_t last_timer = 0;
+uint16_t acpi_status = 1;
+uint16_t acpi_pm1_status = 0;
+uint16_t acpi_pm1_enable = 0;
+uint32_t acpi_last_timer = 0;
 
-uint8_t gpe[4] = { 0, 0, 0, 0 };
+uint8_t acpi_gpe[4] = { 0, 0, 0, 0 };
 
-int last_result = 0;
-uint64_t acpi_start_time;
+int acpi_last_result = 0;
+uint64_t acpi_start_time = 0;
 uint16_t acpi_state[7];
 
+uint32_t acpi_timer_last_value = 0;
+uint32_t acpi_timer_imprecision_offset = 0;
+
 V86_API uint16_t* acpi_get_state() {
-  acpi_state[0] = status;
-  acpi_state[1] = pm1_status;
-  acpi_state[2] = pm1_enable;
-  acpi_state[3] = (uint16_t)gpe[0];
-  acpi_state[4] = (uint16_t)gpe[1];
-  acpi_state[5] = (uint16_t)gpe[2];
-  acpi_state[6] = (uint16_t)gpe[3];
+  acpi_state[0] = acpi_status;
+  acpi_state[1] = acpi_pm1_status;
+  acpi_state[2] = acpi_pm1_enable;
+  acpi_state[3] = (uint16_t)acpi_gpe[0];
+  acpi_state[4] = (uint16_t)acpi_gpe[1];
+  acpi_state[5] = (uint16_t)acpi_gpe[2];
+  acpi_state[6] = (uint16_t)acpi_gpe[3];
   return acpi_state;
 }
 
 V86_API void acpi_set_state(uint16_t* acpi_state) {
-  status = acpi_state[0];
-  pm1_status = acpi_state[1];
-  pm1_enable = acpi_state[2];
-  gpe[0] = (uint8_t)acpi_state[3];
-  gpe[1] = (uint8_t)acpi_state[4];
-  gpe[2] = (uint8_t)acpi_state[5];
-  gpe[3] = (uint8_t)acpi_state[6];
+  acpi_status = acpi_state[0];
+  acpi_pm1_status = acpi_state[1];
+  acpi_pm1_enable = acpi_state[2];
+  acpi_gpe[0] = (uint8_t)acpi_state[3];
+  acpi_gpe[1] = (uint8_t)acpi_state[4];
+  acpi_gpe[2] = (uint8_t)acpi_state[5];
+  acpi_gpe[3] = (uint8_t)acpi_state[6];
 }
 
 V86_API uint64_t acpi_microtick() {
@@ -49,31 +53,44 @@ V86_API uint64_t acpi_microtick() {
 }
 
 V86_API uint32_t acpi_get_timer(uint64_t now) {
-  return (double)now * (double)PMTIMER_FREQ_SECONDS / (double)1000000;
+  uint32_t t = round((double)now * (double)PMTIMER_FREQ_SECONDS / (double)1000000);
+  if (t == acpi_timer_last_value) {
+    if (acpi_timer_imprecision_offset < (uint32_t)PMTIMER_FREQ_SECONDS / (uint32_t)1000000) {
+      acpi_timer_imprecision_offset++;
+    }
+  }
+  else {
+    uint32_t previous_timer = acpi_timer_last_value + acpi_timer_imprecision_offset;
+    if (previous_timer <= t) {
+      acpi_timer_imprecision_offset = 0;
+      acpi_timer_last_value = t;
+    }
+  }
+  return acpi_timer_last_value + acpi_timer_imprecision_offset;
 }
 
 V86_API int acpi_get_result() {
-  return last_result;
+  return acpi_last_result;
 }
 
 V86_API bool acpi_timer(uint64_t now) {
   uint32_t timer = acpi_get_timer(now);
-  bool highest_bit_changed = ((timer ^ last_timer) & (1 << 23)) != 0;
+  bool highest_bit_changed = ((timer ^ acpi_last_timer) & (1 << 23)) != 0;
   bool is_lower = false;
 
-  if ((pm1_enable & 1) && highest_bit_changed) {
-    pm1_status |= 1;
+  if ((acpi_pm1_enable & 1) && highest_bit_changed) {
+    acpi_pm1_status |= 1;
     is_lower = false;
   }
   else {
     is_lower = true;
   }
 
-  if (pm1_enable & 1)
-    last_result = (int)(((uint64_t)0x1000000 - now) / (uint64_t)1000000 * (uint64_t)PMTIMER_FREQ_SECONDS);
+  if (acpi_pm1_enable & 1)
+    acpi_last_result = (int)(((uint64_t)0x1000000 - now) / (uint64_t)1000000 * (uint64_t)PMTIMER_FREQ_SECONDS);
   else
-    last_result = -1;
-  last_timer = timer;
+    acpi_last_result = -1;
+  acpi_last_timer = timer;
 
   return is_lower;
 }
@@ -81,13 +98,13 @@ V86_API bool acpi_timer(uint64_t now) {
 V86_API uint8_t acpi_read8(uint32_t addr) {
   switch (addr) {
     case 0xAFE0:
-      return gpe[0];
+      return acpi_gpe[0];
     case 0xAFE1:
-      return gpe[1];
+      return acpi_gpe[1];
     case 0xAFE2:
-      return gpe[2];
+      return acpi_gpe[2];
     case 0xAFE3:
-      return gpe[3];
+      return acpi_gpe[3];
     default:
       return 0;
   }
@@ -96,11 +113,11 @@ V86_API uint8_t acpi_read8(uint32_t addr) {
 V86_API uint16_t acpi_read16(uint32_t addr) {
   switch (addr) {
     case 0xB000:
-      return pm1_status;
+      return acpi_pm1_status;
     case 0xB002:
-      return pm1_enable;
+      return acpi_pm1_enable;
     case 0xB004:
-      return status;
+      return acpi_status;
     default:
       return 0;
   }
@@ -115,16 +132,16 @@ V86_API uint32_t acpi_read32(uint32_t addr) {
 V86_API void acpi_write8(uint32_t addr, uint8_t value) {
   switch (addr) {
     case 0xAFE0:
-      gpe[0] = value;
+      acpi_gpe[0] = value;
       break;
     case 0xAFE1:
-      gpe[1] = value;
+      acpi_gpe[1] = value;
       break;
     case 0xAFE2:
-      gpe[2] = value;
+      acpi_gpe[2] = value;
       break;
     case 0xAFE3:
-      gpe[3] = value;
+      acpi_gpe[3] = value;
       break;
     default:
       // Maybe add something???
@@ -135,13 +152,13 @@ V86_API void acpi_write8(uint32_t addr, uint8_t value) {
 V86_API void acpi_write16(uint32_t addr, uint16_t value) {
   switch (addr) {
     case 0xB000:
-      pm1_status &= ~value;
+      acpi_pm1_status &= ~value;
       break;
     case 0xB002:
-      pm1_enable = value;
+      acpi_pm1_enable = value;
       break;
     case 0xB004:
-      status = value;
+      acpi_status = value;
       break;
     default:
       // Maybe add something???
